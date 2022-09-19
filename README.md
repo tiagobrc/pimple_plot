cee90ce462ffae93b68ddb228fa44fee511839a5
Tiago Bruno Rezende de Castro <trezende@discovery.rockefeller.edu>
2022-09-19 15:46:55 -0400

    Add Readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# pimple_plot
+A simple code to create easy and nice Volcano Plots from Deseq2 results
--- test_run.R
+++ test_run.R
@@ -5,3 +5,5 @@ plot_volcano(df = as.data.frame(resLFC), padj.sig = 0.05, avg_fc.sig = 0.5,
              name = "condition_treated_vs_untreated",
              highlight_genes = NULL, highlight_top = 10,
              seed = 16)
+
+
